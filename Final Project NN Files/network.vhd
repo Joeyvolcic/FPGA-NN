904aa8fe3cdcd643eaf8d3d11ff98b22891f84ed
Joey Volcic <36684335+Joeyvolcic@users.noreply.github.com>
2023-12-10 22:11:35 -0500

    Update network.vhd

--- Final Project NN Files/network.vhd
+++ Final Project NN Files/network.vhd
@@ -4,6 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 use work.components.all;
 
+
 entity network is
   Port (x ,y: in STD_LOGIC_VECTOR(31 downto 0);
         target_r, target_g, target_b: in STD_LOGIC_VECTOR(31 downto 0);  
@@ -19,8 +20,20 @@ end network;
 
 architecture Behavioral of network is
 
+TYPE init_array is array (0 to 7) of STD_LOGIC_VECTOR(31 downto 0);
+TYPE init_array_2d is array (0 to 5) of init_array;
+
+TYPE hidden_to_last_array_neuron is array (0 to 2) of STD_LOGIC_VECTOR(31 downto 0);
+TYPE hidden_to_last_array_layer is array (0 to 5) of hidden_to_last_array_neuron;
+
+SIGNAL init_array_weights : init_array_2d;
+
+SIGNAL last_layer_weight : hidden_to_last_array_layer;
+SIGNAL last_layer_sensitivity : hidden_to_last_array_layer;
+
 SIGNAL sel_init: STD_LOGIC;
 SIGNAL load_inputs: STD_LOGIC;
+SIGNAL load_hidden: STD_LOGIC;
 
 SIGNAL x_normal: STD_LOGIC_VECTOR(31 downto 0);
 SIGNAL y_normal: STD_LOGIC_VECTOR(31 downto 0);
@@ -40,7 +53,12 @@ SIGNAL predicted_r2_reg: STD_LOGIC_VECTOR(31 downto 0);
 SIGNAL predicted_b2_reg: STD_LOGIC_VECTOR(31 downto 0);
 SIGNAL predicted_g2_reg: STD_LOGIC_VECTOR(31 downto 0);
 
+
 begin
+    init_array_weights(0)(0) <= X"00003832"; init_array_weights(0)(1) <= X"00001734"; init_array_weights(0)(2) <= X"00009584"; init_array_weights(0)(3) <= X"00006594";
+    init_array_weights(0)(4) <= X"00006729"; init_array_weights(0)(5) <= X"00001102"; init_array_weights(0)(6) <= X"00004932"; init_array_weights(0)(7) <= X"00009173";
+
+    
 --NORMILAZATION INPUTS
 --Values will be store prenormalized in ram
 
@@ -55,6 +73,15 @@ register_last_b2: Reg generic map (N => 32) port map ( load => load_inputs, inpu
 
 register_x: Reg generic map (N => 32) port map ( load => load_inputs, input => x, clk => clk, clr => clr, q => x_normal_reg);
 register_y: Reg generic map (N => 32) port map ( load => load_inputs, input => x, clk => clk, clr => clr, q => y_normal_reg);
+
+generate_hidden_neuron: for i in 0 to 5 generate
+  hidden_neuron: Hidden_Layer_Neuron port map(activation_11 => predicted_r_reg, activation_12 => predicted_b_reg, activation_13 => predicted_g_reg, activation_14 => predicted_r2_reg,
+                                              activation_15 => predicted_b2_reg, activation_16 => predicted_g2_reg, activation_17 => predicted_g_reg, activation_18 => predicted_r2_reg,
+                                              initialize_W11 => init_array_weights(i)(0), initialize_W12 => init_array_weights(i)(1),initialize_W13 => init_array_weights(i)(2),initialize_W14 => init_array_weights(i)(3),
+                                              initialize_W15 => init_array_weights(i)(4), initialize_W16 => init_array_weights(i)(5),initialize_W17 => init_array_weights(i)(6),initialize_W18 => init_array_weights(i)(7),
+                                              Load_Wij => load_hidden, sel_init => sel_init, 
+    );
+end generate generate_hidden_neuron;
 
 
 
